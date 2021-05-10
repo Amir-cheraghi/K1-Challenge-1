7ced0540a2517dcdb5cd6575b59fa1588fbdbe53
Amir-cheraghi <chraghizadeh@gmail.com>
2021-05-10 17:45:31 +0430

    Create Delete In Front View

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,4 +1,20 @@
-// import axios from 'axios'
+
+async function deleteTask(button){
+const id = button.attributes["data-url"].nodeValue
+
+const ajax = new XMLHttpRequest()
+ajax.open('DELETE',`http://${location.hostname}:3000/api/tasks/${id}`,true)
+ajax.onreadystatechange = function (){
+    if(this.readyState == 4 && this.status == 200){
+        console.log('deleted')
+        location.reload()
+    }
+}
+ajax.send()
+}
+
+
+
 
 
 
